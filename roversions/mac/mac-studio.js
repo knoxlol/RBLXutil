0b3ca189082f216771b5bc3e8653bfc630d9b447
knox <mathewjaikirk2011@proton.me>
2024-11-21 19:08:55 +0000

    Create mac-studio.js

---
+++ roversions/mac/mac-studio.js
@@ -0,0 +1,18 @@
+// Run in your browser's console
+// Retrives version for the mac port of RobloxStudio.
+const endpoint = 'https://setup.rbxcdn.com/mac/version'; // This is the endpoint for the mac port of RobloxStudio.
+
+fetch(endpoint)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.text();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log('Error fetching data:', error);
+    console.log(`Error fetching data: ${error.message}`);
+  });
